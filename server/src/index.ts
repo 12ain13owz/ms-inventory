6892ae9af39512454c04b9cf02541334307df2e2
Phaiboon Withanthamrong <touchfn@gmail.com>
2024-01-26 14:05:44 +0700

    Project stock-it for universaity

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+
+import log from "./utils/logger";
+import router from "./routes";
+import handlerError from "./middlewares/handler-error.middleware";
+
+const app = express();
+const port = process.env.PORT || 3500;
+
+app.use(morgan("dev"));
+app.use(router);
+app.use(handlerError);
+
+app.listen(port, () => {
+  log.info(`Server listening on port ${port}`);
+});
