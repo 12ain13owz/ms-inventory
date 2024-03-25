3e9ce34edcbcfbc01cdb28f572886201b5bb6172
Phaiboon Withanthamrong <touchfn@gmail.com>
2024-03-25 21:43:06 +0700

    Remove receptchar

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login.routes';
 import { CoreModule } from '../../core/core.module';
-import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from 'ng-recaptcha';
-import { environment } from '../../../environments/environment.development';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [LoginRoutingModule, CoreModule, SharedModule, RecaptchaV3Module],
-  providers: [
-    { provide: RECAPTCHA_V3_SITE_KEY, useValue: environment.recaptcha.siteKey },
-  ],
+  imports: [LoginRoutingModule, CoreModule, SharedModule],
 })
 export class LoginModule {}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
   localhost: 'http://localhost:3000/',
   imageUrl: 'http://localhost:3000/image/',
   apiUrl: 'http://localhost:3000/api/v1/',
-  recaptcha: { siteKey: '6Le73HopAAAAAGRt9es8K3D-Ip1qLy_NQYyZWSfc' },
+  // recaptcha: { siteKey: '6Le73HopAAAAAGRt9es8K3D-Ip1qLy_NQYyZWSfc' },
 };
