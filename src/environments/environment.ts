eade29d7cab30ac4c6b6e94793595d9226a714ec
Phaiboon Withanthamrong <touchfn@gmail.com>
2024-04-24 15:55:52 +0700

    Update enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  localhost: 'https://ms-stock-it.fly.dev/',
+  imageUrl: 'https://ms-stock-it.fly.dev/image/',
+  apiUrl: 'https://ms-stock-it.fly.dev/api/v1/',
+  recaptcha: {
+    siteKey: '6LfEjcMpAAAAAFDtJEtMKeoJL2gvC-1Omkyi_GRO',
+  },
+};
