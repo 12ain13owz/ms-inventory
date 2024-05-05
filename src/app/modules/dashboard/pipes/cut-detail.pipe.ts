984f0f7e5ccac65539eb6c8a5042aa198513b4b3
Phaiboon Withanthamrong <touchfn@gmail.com>
2024-05-05 13:57:46 +0700

    Update display cut pipe

--- src/app/modules/dashboard/pipes/cut-detail.pipe.ts
+++ src/app/modules/dashboard/pipes/cut-detail.pipe.ts
@@ -6,8 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CutDetailPipe implements PipeTransform {
   transform(value: string): string {
     const maxLength = 50; // ยาวสุดที่ต้องการ
+
+    if (value.length < maxLength) return value;
+
     const shortened = value.slice(0, maxLength);
     const lastSpaceIndex = shortened.lastIndexOf(' ');
-    return shortened.slice(0, lastSpaceIndex);
+    return shortened.slice(0, lastSpaceIndex) + ' ...';
   }
 }
