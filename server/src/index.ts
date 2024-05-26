0958655cbea250d9de7b1fb3271ebe0b46d8f21f
Phaiboon Withanthamrong <touchfn@gmail.com>
2024-05-27 02:04:54 +0700

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -65,10 +65,10 @@ app.use(healthRoutes);
 app.use(userRoutesV1);
 app.use(errorHandler);
 
-// app.get('*.*', express.static(path.join(__dirname, '../browser')));
-// app.all('*', (req, res) => {
-//   res.status(200).sendFile('/', { root: 'browser' });
-// });
+app.get('*.*', express.static(path.join(__dirname, '../browser')));
+app.all('*', (req, res) => {
+  res.status(200).sendFile('/', { root: 'browser' });
+});
 
 server.listen(port, async () => {
   await databaseConnect();
